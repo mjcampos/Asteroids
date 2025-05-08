58a6d748710b05cd9be8750131b2fc08874c7c3b
Michael Campos <mjcampos@users.noreply.github.com>
2025-05-08 17:35:03 +0200

    Setup shooting animation

---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class Shooting : MonoBehaviour
+    {
+        [SerializeField] float shootSpeed = 5f;
+
+        float _lastShootSpeed;
+        
+        Animator _animator;
+
+        bool _fireButtonPressed;
+        bool _isShooting;
+        
+        void Start() { 
+            _animator = GetComponent<Animator>();
+            
+            //_animator.SetFloat("SpeedMultiplier", shootSpeed);
+        }
+
+        void OnFire(InputValue value)
+        {
+            _fireButtonPressed = value.isPressed;
+        }
+
+        void Update()
+        {
+            ShootSpeedChangeListener();
+            
+            if (_fireButtonPressed && !_isShooting)
+            {
+                // Mark the player as currently shooting
+                _isShooting = true;
+            
+                Debug.Log("Player is shooting");
+            
+                _animator.SetTrigger("IsShooting");
+            }
+        }
+
+        void NoLongerShooting()
+        {
+            _isShooting = false;
+        }
+
+        void ShootSpeedChangeListener()
+        {
+            if (!Mathf.Approximately(_lastShootSpeed, shootSpeed))
+            {
+                _lastShootSpeed = shootSpeed;
+                
+                _animator.SetFloat("SpeedMultiplier", shootSpeed);
+            }
+        }
+    }
+}
+
