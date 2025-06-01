7fba5d484b870a4c7598aad4178e8e7040bcb1c3
Michael Campos <mjcampos@users.noreply.github.com>
2025-06-01 13:37:59 +0200

    Set up player high score

--- Assets/Scripts/Singletons/ScoreManager.cs
+++ Assets/Scripts/Singletons/ScoreManager.cs
@@ -6,13 +6,14 @@ public class ScoreManager : MonoBehaviour {
     public static ScoreManager Instance { get; private set; }
     
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI highScoreText;
     
     int _score = 0;
+    int _highScore;
     
     void Awake() {
         // Singleton pattern
-        if (Instance != null && Instance != this)
-        {
+        if (Instance != null && Instance != this) {
             Destroy(gameObject);
             return;
         }
@@ -21,11 +22,24 @@ public class ScoreManager : MonoBehaviour {
     }
 
     void Start() {
-        UpdateScore(_score);
+        // Extract score from the player prefs, else set to 0
+        _highScore = PlayerPrefs.GetInt("HighScore", 0);
+        
+        // Set score and high score texts
+        scoreText.text = _score.ToString("D5");
+        highScoreText.text = "High Score: " + _highScore.ToString("D5");
     }
 
     public void UpdateScore(int points) {
+        // Update score
         _score += points;
         scoreText.text = _score.ToString("D5");
+        
+        // Update high score
+        if (_score > _highScore) {
+            _highScore = _score;
+            highScoreText.text = "High Score: " + _highScore.ToString("D5");
+            PlayerPrefs.SetInt("HighScore", _highScore);
+        }
     }
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,7 +1,20 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI highScoreText;
+
+    void Start()
+    {
+        // Extract score from the player prefs, else set to 0
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        
+        // Set high score
+        highScoreText.text = "High Score: " + highScore.ToString("D5");
+    }
+
     public void LoadGame() {
         SceneManager.LoadScene("Game");
     }
